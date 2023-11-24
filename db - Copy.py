8623e7550ed211bd242042e15683a1821db0f70e
Your Name <you@example.com>
2023-11-24 16:39:46 +0700

    crud and html css

--- db - Copy.py
+++ db - Copy.py
@@ -1,14 +1,14 @@
-from sqlalchemy import create_engine, Column, Integer, String
+from sqlalchemy import create_engine, Column, Integer, String, and_
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-engine = create_engine('postgresql://remove:123456@192.168.1.6:5432/mydb1')
+engine = create_engine('postgresql://thutrang:123456@localhost:5432/mydb1')
 # create 1 obj Base
 Base = declarative_base()
 
 #define 1 table
 class Person(Base):
-    __table_args__ = {'schema': 'autotest'}
-    __tablename__= 'person'
+    __table_args__ = {'schema': 'db1'}
+    __tablename__= 'Person'
     id = Column(Integer, primary_key=True)
     user_name = Column(String)
     email = Column(String, unique=True, nullable=False)
@@ -17,7 +17,7 @@ class Person(Base):
     department_id = Column(Integer)
 
 class Department(Base):
-    __table_args__ = {'schema': 'autotest'}
+    __table_args__ = {'schema': 'db1'}
     __tablename__ = 'department'
     id = Column(Integer, primary_key=True)
     name = Column(String, unique=True)
@@ -27,19 +27,42 @@ Session = sessionmaker(bind=engine)
 session = Session()
 
 #Function insert data into table
-def insert_data(id, user_name, email, address, position, department_id):
+def insert_person(id, user_name, email, address, position, department_id):
     #create object (= constructor)
     new_record = Person(id=id, user_name=user_name, email=email, address=address, position=position, department_id=department_id)
     #add in session
     session.add(new_record)
     #commit -> save data in database
     session.commit()
+    return new_record
 
 #Function get data according to condition
 def get_person_by_id(id):
     # use filter
     query = session.query(Person).filter(Person.id == id)
     #return list persons
-    return query.all()
-# def create_person(Person):
+    return query.first()
 
+#function get all person
+def get_all_person():
+    query = session.query(Person).filter()
+    return query.all()
+
+# function update_person(Person):
+def update_user_name_by_id(id):
+    data = get_person_by_id(id)
+    if (data == None):
+        print('id not exists!!!')
+    else:
+        query = session.query(Person).filter(Person.id == id).update({
+            'user_name': 'Ann'
+        }, synchronize_session=False)
+        person = session.query(Person).filter(Person.id == id)
+        session.commit()
+        print('after update: ', Person.id, Person.user_name, Person.email)
+
+#functon delete person by id
+def delete_person_by_id(id):
+    query = session.query(Person).filter(Person.id == id).delete()
+    session.commit()
+    print("delete success!")
--- index.py
+++ index.py
@@ -1,3 +1,33 @@
-import json
-import socket
-from db import Person, Department, get_person_by_id
+from flask import Flask, render_template
+from db import (Person, Department, insert_person, get_person_by_id, update_user_name_by_id, delete_person_by_id, get_all_person)
+
+app = Flask(__name__)
+@app.route("/")
+def home():
+    return render_template('index.html')
+
+if __name__ == '__main__':
+
+    #get person by id
+    # data = get_person_by_id(2)
+    # if(data != None):
+    #     print('this is person have id = 2: ' + data.user_name)
+    # else:
+    #     print('data not exists!!!')
+
+    #update person
+    # update_user_name_by_id(2)
+
+    #delete person
+    # del_person = delete_person_by_id(2)
+
+    #create person
+    # new_person = insert_person(7, 'HoangAnhTu', 'tuhoanganh@gmail.com', 'Bc Giang', 'IT Comtor', 3)
+    # print(new_person.user_name)
+
+    #get all person
+    list_person = get_all_person()
+    for x in list_person:
+        print(x.user_name)
+
+    app.run(debug=True)
